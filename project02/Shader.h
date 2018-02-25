f8c45d473fc30c8a2aa16ca818f3d9b5ef916a34
liqilei <blighli@gmail.com>
2018-02-25 21:57:49 +0800

    add project02 use shader

---
+++ project02/Shader.h
@@ -0,0 +1,12 @@
+//
+// Created by liqilei on 2018/2/25.
+//
+
+#ifndef PROJECT_SHADER_H
+#define PROJECT_SHADER_H
+
+#include <string>
+
+std::string ReadFile(std::string filename);
+
+#endif //PROJECT_SHADER_H
